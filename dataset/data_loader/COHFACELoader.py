7dca7e3a464a4d82d718473a697b35f40cecf478
b-acharya <bhargav.acharya@uni-bielefeld.de>
2023-08-29 15:53:33 +0200

    addition of COHFACE dataset

--- dataset/data_loader/COHFACELoader.py
+++ dataset/data_loader/COHFACELoader.py
@@ -12,6 +12,8 @@ import re
 import cv2
 import h5py
 import numpy as np
+
+import config
 from dataset.data_loader.BaseLoader import BaseLoader
 
 
@@ -46,41 +48,108 @@ class COHFACELoader(BaseLoader):
                 name(str): name of the dataloader.
                 config_data(CfgNode): data settings(ref:config.py).
         """
+        if name == "train":
+            self.split_path = config_data.SPLIT_PATH
+        elif name == "valid":
+            self.split_path = config_data.SPLIT_PATH
+        elif name == "test":
+            self.split_path = config_data.SPLIT_PATH
+
+        if self.split_path == None:
+            self.use_predefined_splits = False
+        else:
+            self.use_predefined_splits = True
+
         super().__init__(name, data_path, config_data)
+
+    def _read_split_path(self):
+        data_paths = []
+        with open(self.split_path, 'r') as f:
+            for line in f.readlines():
+                data_paths.append(self.raw_data_path + line.strip())
+        return data_paths
 
     def get_raw_data(self, data_path):
         """Returns data directories under the path(For COHFACE dataset)."""
-        data_dirs = glob.glob(data_path + os.sep + "*")
+        dirs = list()
+        if self.use_predefined_splits:
+            data_dirs = self._read_split_path()
+            for data_dir in data_dirs:
+                subject = data_dir.split('/')[-3]
+                i = data_dir.split('/')[-2]
+                dirs.append({"index": int('{0}0{1}'.format(subject, i)),
+                             "path": os.path.join(data_dir)})
+
+        else:
+            data_dirs = glob.glob(data_path + os.sep + "*")
+            for data_dir in data_dirs:
+                for i in range(4):
+                    subject = os.path.split(data_dir)[-1]
+                    dirs.append({"index": int('{0}0{1}'.format(subject, i)),
+                             "path": os.path.join(data_dir, str(i))})
         if not data_dirs:
             raise ValueError(self.dataset_name + " data paths empty!")
-        dirs = list()
-        for data_dir in data_dirs:
-            for i in range(4):
-                subject = os.path.split(data_dir)[-1]
-                dirs.append({"index": int('{0}0{1}'.format(subject, i)),
-                             "path": os.path.join(data_dir, str(i))})
         return dirs
 
-    def preprocess_dataset(self, data_dirs, config_preprocess):
+    def split_raw_data(self, data_dirs, begin, end):
+        """Returns a subset of data dirs, split with begin and end values,
+        and ensures no overlapping subjects between splits"""
+        if self.use_predefined_splits:
+            return  data_dirs
+        # return the full directory
+        if begin == 0 and end == 1:
+            return data_dirs
+
+        # get info about the dataset: subject list and num vids per subject
+        data_info = dict()
+        for data in data_dirs:
+            subject = data['subject']
+            data_dir = data['path']
+            index = data['index']
+            # creates a dictionary of data_dirs indexed by subject number
+            if subject not in data_info:  # if subject not in the data info dictionary
+                data_info[subject] = []  # make an emplty list for that subject
+            # append a tuple of the filename, subject num, trial num, and chunk num
+            data_info[subject].append({"index": index, "path": data_dir, "subject": subject})
+
+        subj_list = list(data_info.keys())  # all subjects by number ID (1-27)
+        subj_list = sorted(subj_list)
+        num_subjs = len(subj_list)  # number of unique subjects
+
+        # get split of data set (depending on start / end)
+        subj_range = list(range(0, num_subjs))
+        if begin != 0 or end != 1:
+            subj_range = list(range(int(begin * num_subjs), int(end * num_subjs)))
+
+        # compile file list
+        data_dirs_new = []
+        for i in subj_range:
+            subj_num = subj_list[i]
+            subj_files = data_info[subj_num]
+            data_dirs_new += subj_files  # add file information to file_list (tuple of fname, subj ID, trial num,
+            # chunk num)
+
+        return data_dirs_new
+    def preprocess_dataset_subprocess(self, data_dirs, config_preprocess, i, file_list_dict):
         """Preprocesses the raw data."""
-        file_num = len(data_dirs)
-        for i in range(file_num):
-            frames = self.read_video(
-                os.path.join(
-                    data_dirs[i]["path"],
-                    "data.avi"))
-            bvps = self.read_wave(
-                os.path.join(
-                    data_dirs[i]["path"],
-                    "data.hdf5"))
-            target_length = frames.shape[0]
-            bvps = BaseLoader.resample_ppg(bvps, target_length)
-            frames_clips, bvps_clips = self.preprocess(frames, bvps, config_preprocess)
-            self.preprocessed_data_len += self.save(frames_clips, bvps_clips, data_dirs[i]["index"])
+        filename = os.path.split(data_dirs[i]['path'])[-1]
+        saved_filename = data_dirs[i]['index']
+        frames = self.read_video(
+                data_dirs[i]["path"] + ".avi")
+        bvps = self.read_wave(
+                data_dirs[i]["path"] + ".hdf5")
+        print(frames.shape)
+        print(data_dirs[i]['path'])
+        target_length = frames.shape[0]
+        bvps = BaseLoader.resample_ppg(bvps, target_length)
+        frames_clips, bvps_clips = self.preprocess(frames, bvps, config_preprocess)
+        input_name_list, label_name_list = self.save_multi_process(frames_clips, bvps_clips, saved_filename)
+        file_list_dict[i] = input_name_list
 
     @staticmethod
     def read_video(video_file):
         """Reads a video file, returns frames(T,H,W,3) """
+        print("start of read_video")
         VidObj = cv2.VideoCapture(video_file)
         VidObj.set(cv2.CAP_PROP_POS_MSEC, 0)
         success, frame = VidObj.read()
@@ -88,10 +157,11 @@ class COHFACELoader(BaseLoader):
         while (success):
             frame = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2RGB)
             frame = np.asarray(frame)
-            frame[np.isnan(frame)] = 0  # TODO: maybe change into avg
+            if np.isnan(frame).any():
+                frame[np.isnan(frame)] = 0  # TODO: maybe change into avg
             frames.append(frame)
             success, frame = VidObj.read()
-
+        print("end of read_video")
         return np.asarray(frames)
 
     @staticmethod
