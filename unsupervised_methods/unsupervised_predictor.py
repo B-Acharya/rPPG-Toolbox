8e645881d19cb3bcdd1503a5d7c0b99d548f0354
b-acharya <bhargav.acharya@uni-bielefeld.de>
2024-08-16 10:26:02 +0200

    DST processing update and clean up

--- unsupervised_methods/unsupervised_predictor.py
+++ unsupervised_methods/unsupervised_predictor.py
@@ -16,67 +16,19 @@ from unsupervised_methods.methods.LGI import *
 from unsupervised_methods.methods.PBV import *
 from unsupervised_methods.methods.POS_WANG import *
 from evaluation.BlandAltmanPy import BlandAltman
+from evaluation.metrics import metrics_calculations
 from tqdm import tqdm
+import random
 import pickle
+from unsupervised_methods.utils import ecg_processing
 
 
-def metrics_calculations(ground_truth, predictions, SNR, config, logger):
-
-    predictions = np.array(predictions)
-    ground_truth = np.array(ground_truth)
-    SNR = np.array(SNR)
-    num_test_samples = len(predictions)
-    method = config.INFERENCE.EVALUATION_METHOD
-    filename_id = config.TRAIN.MODEL_FILE_NAME
-    for metric in config.UNSUPERVISED.METRICS:
-        if metric == "MAE":
-            MAE = np.mean(np.abs(predictions - ground_truth))
-            standard_error = np.std(np.abs(predictions - ground_truth)) / np.sqrt(num_test_samples)
-            print("{2} MAE : {0} +/- {1}".format(MAE, standard_error, method))
-            logger.log_metrics({"MAE": MAE})
-        elif metric == "RMSE":
-            RMSE = np.sqrt(np.mean(np.square(predictions - ground_truth)))
-            standard_error = np.std(np.square(predictions - ground_truth)) / np.sqrt(num_test_samples)
-            print("{2} RMSE : {0} +/- {1}".format(RMSE, standard_error, method))
-            logger.log_metrics({"RMSE": RMSE})
-        elif metric == "MAPE":
-            MAPE = np.mean(np.abs((predictions - ground_truth) / ground_truth)) * 100
-            standard_error = np.std(np.abs((predictions - ground_truth) / ground_truth)) / np.sqrt(
-                num_test_samples) * 100
-            print("{2} MAPE : {0} +/- {1}".format(MAPE, standard_error, method))
-            logger.log_metrics({"MAPE": MAPE})
-        elif metric == "Pearson":
-            Pearson = np.corrcoef(predictions, ground_truth)
-            correlation_coefficient = Pearson[0][1]
-            standard_error = np.sqrt((1 - correlation_coefficient ** 2) / (num_test_samples - 2))
-            print("{2} Pearson : {0} +/- {1}".format(correlation_coefficient, standard_error, method))
-            logger.log_metrics({"Pearson": Pearson})
-        elif metric == "SNR":
-            SNR = np.mean(SNR)
-            standard_error = np.std(SNR) / np.sqrt(num_test_samples)
-            print("{2} SNR : {0} +/- {1}".format(SNR, standard_error, method))
-            logger.log_metrics({"SNR": SNR})
-        else:
-            raise ValueError("Wrong Test Metric Type")
-    compare = BlandAltman(ground_truth, predictions, config, logger=logger, averaged=True)
-    compare.scatter_plot(
-        x_label='GT PPG HR [bpm]',
-        y_label='rPPG HR [bpm]',
-        show_legend=True, figure_size=(5, 5),
-        the_title=f'{filename_id}_peak_BlandAltman_ScatterPlot',
-        file_name=f'{filename_id}_peak_BlandAltman_ScatterPlot.pdf')
-    compare.difference_plot(
-        x_label='Difference between rPPG HR and GT PPG HR [bpm]',
-        y_label='Average of rPPG HR and GT PPG HR [bpm]',
-        show_legend=True, figure_size=(5, 5),
-        the_title=f'{filename_id}_peak_BlandAltman_DifferencePlot',
-        file_name=f'{filename_id}_peak_BlandAltman_DifferencePlot.pdf')
 
 def save_test_outputs( predictions, labels, config, method_name):
     if config.TOOLBOX_MODE == 'train_and_test' or config.TOOLBOX_MODE == 'only_test':
         output_dir = config.TEST.OUTPUT_SAVE_DIR
     else:
-        output_dir = config.UNSUPERVISED.DATA.CACHED_PATH
+        output_dir = config.UNSUPERVISED.OUT_SAVE_DIR
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
@@ -89,7 +41,6 @@ def save_test_outputs( predictions, labels, config, method_name):
         filename_id = model_file_root + "_" + config.TEST.DATA.DATASET
     elif config.TOOLBOX_MODE == 'unsupervised_method':
         filename_id = method_name
-
     else:
         raise ValueError('Metrics.py evaluation only supports train_and_test and only_test!')
     output_path = os.path.join(output_dir, filename_id + '_outputs.pickle')
@@ -132,12 +83,32 @@ def unsupervised_predict(config, data_loader, method_name, logger, log=True, sav
     for _, test_batch in enumerate(sbar):
         batch_size = test_batch[0].shape[0]
         for idx in range(batch_size):
-            if DST:
-                data_input, gt_hr = test_batch[0][idx].cpu().numpy(), test_batch[1][idx].cpu().numpy()
-            else:
-                data_input, labels_input = test_batch[0][idx].cpu().numpy(), test_batch[1][idx].cpu().numpy()
-
+            data_input, labels_input = test_batch[0][idx].cpu().numpy(), test_batch[1][idx].cpu().numpy()
             filename = test_batch[2][idx]
+            if len(labels_input) == 0:
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("ECG data missing, please check", filename)
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                continue
+            if data_input.shape[0] == 0:
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("Video data missing, please check", filename)
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                print("--------------")
+                continue
             index = test_batch[2]
             MAE_per_scenarios = dict()
             if method_name == "POS":
@@ -148,56 +119,95 @@ def unsupervised_predict(config, data_loader, method_name, logger, log=True, sav
                 BVP = ICA_POH(data_input, config.UNSUPERVISED.DATA.FS)
             elif method_name == "GREEN":
                 BVP = GREEN(data_input)
+            elif method_name == "RED":
+                BVP = RED(data_input)
+            elif method_name == "BLUE":
+                BVP = BLUE(data_input)
             elif method_name == "LGI":
                 BVP = LGI(data_input)
             elif method_name == "PBV":
                 BVP = PBV(data_input)
-            elif method_name == "dummy":
-                if DST:
-                    pass
-                else:
-                    BVP = labels_input
+            elif method_name == "dummy" or method_name == "random":
+                BVP = labels_input
             else:
                 raise ValueError("unsupervised method name wrong!")
 
             if save_outputs:
                 predictions[filename] = BVP
-                if DST:
-                    labels[filename] = gt_hr
+                print(BVP.shape)
+                labels[filename] = labels_input
+
+            if DST:
+                #TODO: should be changed and moved to dataloader, bit hacky
+                label_frame_size = len(labels_input)
+                if label_frame_size>25000 and label_frame_size<30000:
+                    sampling_rate = 300
                 else:
-                    labels[filename] = labels_input
+                    sampling_rate = 1000
 
             video_frame_size = test_batch[0].shape[1]
             if config.INFERENCE.EVALUATION_WINDOW.USE_SMALLER_WINDOW:
                 window_frame_size = config.INFERENCE.EVALUATION_WINDOW.WINDOW_SIZE * config.UNSUPERVISED.DATA.FS
                 if window_frame_size > video_frame_size:
                     window_frame_size = video_frame_size
+                if DST:
+                    label_frame_size = config.INFERENCE.EVALUATION_WINDOW.WINDOW_SIZE * sampling_rate
+                    if method_name == "dummy" or method_name == "random":
+                        window_frame_size = label_frame_size
             else:
+                # if method_name == "dummy" or method_name == "random":
+                #     window_frame_size = label_frame_size
+                # else:
                 window_frame_size = video_frame_size
+
+
+
 
             for i in range(0, len(BVP), window_frame_size):
                 BVP_window = BVP[i:i+window_frame_size]
+
                 if DST:
-                    pass
+                    #conversion of number of samples to match the same window (BVP -> ECG )
+                    if method_name == "dummy" or method_name == "random":
+                        label_window = labels_input[i:i + label_frame_size]
+                    else:
+                        j = (i//config.UNSUPERVISED.DATA.FS) * sampling_rate
+                        label_window = labels_input[j:j + label_frame_size]
                 else:
-                    label_window = labels_input[i:i+window_frame_size]
+                    label_window = labels_input[i:i + window_frame_size]
 
                 if len(BVP_window) < 9:
                     print(f"Window frame size of {len(BVP_window)} is smaller than minimum pad length of 9. Window ignored!")
                     continue
 
+                if DST and len(BVP_window)< window_frame_size//2:
+                    print(f"Window smaller than the smaller window size. Window ignored")
+                    continue
+
                 if config.INFERENCE.EVALUATION_METHOD == "peak detection":
                     if DST:
                         pre_hr = calculate_HR(BVP_window, fs=config.UNSUPERVISED.DATA.FS, diff_flag=False, hr_method='Peak')
+                        gt_hr = ecg_processing(label_window, sampling_rate=sampling_rate)
                     else:
                         gt_hr, pre_hr, SNR = calculate_metric_per_video(BVP_window, label_window, diff_flag=False,
                                                                     fs=config.UNSUPERVISED.DATA.FS, hr_method='Peak')
                 elif config.INFERENCE.EVALUATION_METHOD == "FFT":
-                    gt_hr, pre_hr, SNR = calculate_metric_per_video(BVP_window, label_window, diff_flag=False,
+                    if DST:
+                        pre_hr = calculate_HR(BVP_window, fs=config.UNSUPERVISED.DATA.FS, diff_flag=False, hr_method='FFT')
+                        gt_hr = ecg_processing(label_window, sampling_rate=sampling_rate)
+                    else:
+                        gt_hr, pre_hr, SNR = calculate_metric_per_video(BVP_window, label_window, diff_flag=False,
                                                                             fs=config.UNSUPERVISED.DATA.FS,
                                                                             hr_method='FFT')
                 elif config.INFERENCE.EVALUATION_METHOD == "Welch":
-                    gt_hr, pre_hr, SNR = calculate_metric_per_video(BVP_window, label_window, diff_flag=False,
+                    if DST:
+                        if method_name == "dummy" or method_name == "random":
+                            pre_hr = ecg_processing(label_window, sampling_rate=sampling_rate)
+                        else:
+                            pre_hr = calculate_HR(BVP_window, fs=config.UNSUPERVISED.DATA.FS, diff_flag=False, hr_method='Welch')
+                        gt_hr = ecg_processing(label_window, sampling_rate=sampling_rate)
+                    else:
+                        gt_hr, pre_hr, SNR = calculate_metric_per_video(BVP_window, label_window, diff_flag=False,
                                                                             fs=config.UNSUPERVISED.DATA.FS,
                                                                             hr_method='Welch')
                 else:
@@ -205,10 +215,17 @@ def unsupervised_predict(config, data_loader, method_name, logger, log=True, sav
 
                 gt_hr_all.append(gt_hr)
                 predict_hr_all.append(pre_hr)
-                SNR_all.append(SNR)
+                if not DST:
+                    SNR_all.append(SNR)
 
                 #creating a dict for further processing
-                predictions_dict[filename] = {"GT_HR": gt_hr, "Pred_HR": pre_hr}
+                if config.INFERENCE.EVALUATION_WINDOW.USE_SMALLER_WINDOW:
+                    if method_name == "dummy" or method_name == "random":
+                        predictions_dict[filename + str(i//sampling_rate)] = {"GT_HR": gt_hr, "Pred_HR": pre_hr}
+                    else:
+                        predictions_dict[filename + str(i // config.UNSUPERVISED.DATA.FS)] = {"GT_HR": gt_hr, "Pred_HR": pre_hr}
+                else:
+                    predictions_dict[filename ] = {"GT_HR": gt_hr, "Pred_HR": pre_hr}
 
                 #MAE for each segment
                 MAE = np.mean(np.abs(gt_hr - pre_hr))
@@ -229,17 +246,26 @@ def unsupervised_predict(config, data_loader, method_name, logger, log=True, sav
             predictions_dict[key]["Pred_HR"] = mean_hr
         for i in range(len(predict_hr_all)):
             predict_hr_all[i] = mean_hr
+    if method_name == "random":
+        print("running random predicitons")
+        min, max = np.int(np.min(gt_hr_all)),np.int(np.max(gt_hr_all))
+        print(min, max)
+        print(len(predict_hr_all), len(predictions_dict.keys()))
+        for i, key in enumerate(predictions_dict.keys()):
+            sample = random.sample(range(min, max), 1)[0]
+            predictions_dict[key]["Pred_HR"] = sample
+            predict_hr_all[i] = sample
+
     if save_outputs:
         save_test_outputs(predictions, labels, config, method_name)
 
     print("Used Unsupervised Method: " + method_name)
     dataframe = pd.DataFrame.from_dict(predictions_dict).T
-    dataframe.to_csv(f"{config.UNSUPERVISED.DATA.CACHED_PATH}/{method_name}_{config.INFERENCE.EVALUATION_METHOD}.csv")
+    dataframe.to_csv(f"{config.UNSUPERVISED.OUT_SAVE_DIR}/{method_name}_{config.INFERENCE.EVALUATION_METHOD}.csv")
     logger.experiment.log_dataframe_profile(dataframe, "whole-data")
     dataframe['GT_HR'].plot.hist()
     logger.experiment.log_figure(figure=plt, figure_name="GT-Histogram")
     plt.close()
-
 
     metrics_calculations(gt_hr_all, predict_hr_all, SNR_all, config, logger)
 
